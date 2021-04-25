0cd3187ea183dec974e50c14f59511404f41afc0
EnzoPB <enzo@zaptoinc.com>
2021-04-26 01:40:54 +0200

    manageButton.js cleanup + comment

--- www/assets/js/manageButton.js
+++ www/assets/js/manageButton.js
@@ -10,7 +10,7 @@ var db = {
 	buttons: new nedb({ filename: path.join(remote.app.getPath('userData'), 'buttons.db'), autoload: true })
 };
 
-var button = {
+var button = { // initial button data
 	id: 0,
 	name: '',
 	color: '#000000',
@@ -20,38 +20,37 @@ var button = {
 
 $(() => {
 	button.id = ipcRenderer.sendSync('getButton');
-	db.buttons.find({ id: button.id }, (err, _button) => {
+	db.buttons.find({ id: button.id }, (err, _button) => { // we search the databse for existing data
 		if (err) throw err;
 
-		if (_button.length == 1) {
+		if (_button.length == 1) { // if the button already exists, we pre-fill the fields with the existing data
 			button = _button[0];
 			$('#name').val(button.name);
 			$('#color').val(button.color);
-			$('#action').val(button.action);
-		} else {
-			$('#deleteButton').hide();
-			$('#action').val('');
+		} else { // if the button doesn't exist we let the fields empty
+			$('#deleteButton').hide(); // and we hide the "delete" button
 		}
 
-		updateAction();
-		$('.loader').hide();
+		updateAction(); // update the action data
+		$('.loader').hide(); // hide the loader
 	});
 
-	$('#name').on('change', event => {
+	$('#name').on('change', () => { // if the name changes, we save it into the button variable
 		button.name = $('#name').val();
 	});
 
-	$('#color').on('input', event => {
+	$('#color').on('input', () => { // if the color changes (in real-time in the color-picker), we send the data to the arduino to preview the color on the device
 		ipcRenderer.send('changeBtnColor', $('#color').val());
 	});
-	$('#color').on('change', event => {
-		button.color = $('#color').val();
+	$('#color').on('change', () => {
+		button.color = $('#color').val(); // if the color changes, we save it into the button variable
 	});
 
-	$('#action').on('change', updateAction);
+	$('#action').on('change', updateAction); // if the action changes, we update the action data
 
-	$('#submitButton').on('click', () => {
+	$('#submitButton').on('click', () => { // if the user click the submit button, we save the changes or we create the new button
 
+		// we check the form for empty input
 		var emptyInput = [];
 		if ($('#name').val() == '') {
 			emptyInput.push('name');
@@ -78,38 +77,39 @@ $(() => {
 				break;
 		}
 
-		if (emptyInput.length != 0) {
-			dialog.showMessageBox({
+		if (emptyInput.length != 0) { // if there is one or more empty input
+			dialog.showMessageBox({ // we show a warning message
 				type: 'warning',
-				message: `You must fill the following fields: ${emptyInput.join(', ')}`
+				message: `You must fill the following fields: ${emptyInput.join(', ')}` // telling which input is empty
 			});
 		} else {
-			db.buttons.find({ id: button.id }, (err, count) => {
+			db.buttons.find({ id: button.id }, (err, count) => { // we search the database for existing data
 				if (err) throw err;
-				if (count == 0) {
-					db.buttons.insert(button, (err, a) => {
+
+				if (count == 0) { // if the buttons doesn't exists in the database
+					db.buttons.insert(button, (err, a) => { // we create the entry
 						if (err) throw err;
-						window.close();
+						window.close(); // and we close the window
 					});
-				} else {
-					db.buttons.update({ id: button.id }, button, (err, a) => {
+				} else { // if the button already exists
+					db.buttons.update({ id: button.id }, button, (err, a) => { // we update the entry
 						if (err) throw err;
-						window.close();
+						window.close(); // and we close the window
 					})
 				}
 			});
 		}
 	});
 
-	$('#deleteButton').on('click', event => {
-		if (dialog.showMessageBoxSync({
+	$('#deleteButton').on('click', () => { // if the "delete button is clicked"
+		if (dialog.showMessageBoxSync({ // we ask the user for a confirmation
 				type: 'question',
 				title: 'Warning',
 				message: `Are you sure you want to delete "${button.name}"?`,
 				detail: 'This action is irreversible',
 				buttons: ['No', 'Yes']
-			}) == 1) {
-			db.buttons.remove({ id: button.id }, err => {
+			}) == 1) { // if he awnser "yes"
+			db.buttons.remove({ id: button.id }, err => { // we delete the entry
 				if (err) throw err;
 				window.close();
 			});
@@ -120,10 +120,10 @@ $(() => {
 var actionDataListeners = [];
 const updateAction = () => {
 	var actionData = $('#actionData');
-	actionData.html('');
+	actionData.html(''); // we remove the action data fields
 
 	actionDataListeners.forEach(listener => {
-		listener.element.off(listener.event);
+		listener.element.off(listener.event); // we remove every event listener
 	});
 	button.action = $('#action').val();
 
@@ -192,7 +192,7 @@ const updateAction = () => {
 			break;
 	}
 
-	for (i in button.actionData) {
-		$(`#${i}`).val(button.actionData[i]);
+	for (i in button.actionData) { // for every action data field
+		$(`#${i}`).val(button.actionData[i]); // we auto-fill with the action data value
 	}
 }

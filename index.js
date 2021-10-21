dc30baae87fb050f252dcfe99d87b184af8e564d
Enzo <nelox@mail.com>
2021-10-21 21:44:02 +0200

    forgot this in recent commits

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function createAudioEngineWindow() {
 	audioEngineWindow.loadFile('audioEngine/audioEngine.html'); // load the html document
 }
 
-function createMainWindow() {
+function createMainWindow(callback) {
 	log('Creating main window');
 	mainWindow = new BrowserWindow({ // Create the main window
 		width: 1000,
